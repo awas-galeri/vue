1f57dccbb0f6027b97a1242d6f0539ed91e6fb9d
AWAS Galeri <awasgaleri@gmail.com>
2023-07-25 15:43:25 +0700

    update learn create data

---
+++ src/components/CreatePage.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="container mb-3">
+    <form action="">
+      <div class="row">
+        <div class="col-md-8">
+          <div class="mb-3">
+            <label for="" class="form-label">Page Title</label>
+            <input type="text" class="form-control" v-model="pageTitle" />
+          </div>
+          <div class="mb-3">
+            <label for="" class="form-label">Content</label>
+            <textarea
+              class="form-control"
+              rows="5"
+              type="text"
+              v-model="content"
+            ></textarea>
+          </div>
+        </div>
+        <div class="col">
+          <div class="mb-3">
+            <label for="" class="form-label">Link Text</label>
+            <input type="text" class="form-control" v-model="linkText" />
+          </div>
+          <div class="mb-3">
+            <label for="" class="form-label">Link URL</label>
+            <input type="text" class="form-control" v-model="linkUrl" />
+          </div>
+          <div class="row mb-3">
+            <div class="form-check ms-3">
+              <input type="checkbox" class="form-check-input" />
+              <label class="form-check-label" for="gridCheck1">Published</label>
+            </div>
+          </div>
+        </div>
+
+        <div class="mb-3">
+          <button
+            class="btn btn-secondary"
+            :disabled="isFormInvalid"
+            @click.prevent="submitForm"
+          >
+            Create Page
+          </button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["pageCreated"],
+  computed: {
+    isFormInvalid() {
+      return (
+        !this.pageTitle || !this.content || !this.linkText || !this.linkUrl
+      );
+    },
+  },
+  data() {
+    return {
+      pageTitle: "",
+      content: "",
+      linkText: "",
+      linkUrl: "",
+    };
+  },
+  methods: {
+    submitForm() {
+      if (!this.pageTitle || !this.content || !this.linkText || !this.linkUrl) {
+        alert("Tulung diisi sing bener");
+        return;
+      }
+      this.pageCreated({
+        pageTitle: this.pageTitle,
+        content: this.content,
+        link: {
+          text: this.linkText,
+          url: this.linkUrl,
+        },
+      });
+    },
+  },
+};
+</script>
